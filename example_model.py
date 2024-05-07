a027dd8d40eb760836a9e54828db92e5f296a5c3
Ezozbek <cssezozbel@gmail.com>
2024-05-07 23:53:06 +0500

    dwdw

---
+++ example_model.py
@@ -0,0 +1,20 @@
+from pygenuz.db import *
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False)
+    email = Column(String(100), nullable=False, unique=True)
+    age = Column(Integer)
+    books_count = Column(Integer)
+    is_active = Column(Boolean, default=True)
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    author = Column(String(100), nullable=False)
+    published_date = Column(DateTime)
